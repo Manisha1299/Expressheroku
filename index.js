581dab662ef01e857e906d8916861940fab7bd7a
Manisha1299 <manishamore1299@gmail.com>
2022-08-17 17:35:03 +0530

    new update

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const data = require("./student/getDetails.json");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 1010
+app.get("/",(req,res)=>{
+    res.send("api is running..");
+})
 
 app.post("/student/getDetails", function (req, res) {
     res.send(data);
